ab96a8966ddfafd7b912c0eed8ff593342fb0331
swivet-effect <dy.fry203@gmail.com>
2026-02-27 13:37:54 +1300

    Dash Mechanic - Removed Speed Calc

--- player_movement.gd
+++ player_movement.gd
@@ -5,58 +5,87 @@ var horizontal = 0
 var long = 0
 var toBeNormalized
 
-var oldPos = 0
-var curPos = 0
-var distanceTraveled
-var movementSpeedCalc
-
-func _ready():
-	$"Speedometer Timer".start()
+var dashing = false
 
 func _physics_process(_delta):	
-	if Input.is_action_just_pressed("ui_left"):
-		horizontal = -1
-	elif horizontal == -1 and Input.is_action_just_released("ui_left"): 
-		if Input.is_action_pressed("ui_right"):
-			horizontal = 1
-		else:
-			horizontal = 0
-	
-	if Input.is_action_just_pressed("ui_right"):
-		horizontal = 1
-	elif horizontal == 1 and Input.is_action_just_released("ui_right"): 
-		if Input.is_action_pressed("ui_left"):
+	if dashing == false:
+		if Input.is_action_just_pressed("ui_left"):
 			horizontal = -1
-		else:
-			horizontal = 0
-
-	if Input.is_action_just_pressed("ui_down"):
-		long = 1
-	elif long == 1 and Input.is_action_just_released("ui_down"): 
-		if Input.is_action_pressed("ui_down"):
-			long = -1
-		else:
-			long = 0
-	
-	if Input.is_action_just_pressed("ui_up"):
-		long = -1
-	elif long == -1 and Input.is_action_just_released("ui_up"): 
-		if Input.is_action_pressed("ui_up"):
+		elif horizontal == -1 and Input.is_action_just_released("ui_left"): 
+			if Input.is_action_pressed("ui_right"):
+				horizontal = 1
+			else:
+				horizontal = 0
+		
+		if Input.is_action_just_pressed("ui_right"):
+			horizontal = 1
+		elif horizontal == 1 and Input.is_action_just_released("ui_right"): 
+			if Input.is_action_pressed("ui_left"):
+				horizontal = -1
+			else:
+				horizontal = 0
+		
+		if Input.is_action_just_pressed("ui_down"):
 			long = 1
-		else:
-			long = 0
+		elif long == 1 and Input.is_action_just_released("ui_down"): 
+			if Input.is_action_pressed("ui_down"):
+				long = -1
+			else:
+				long = 0
+		
+		if Input.is_action_just_pressed("ui_up"):
+			long = -1
+		elif long == -1 and Input.is_action_just_released("ui_up"): 
+			if Input.is_action_pressed("ui_up"):
+				long = 1
+			else:
+				long = 0
+		
+		if Input.is_action_just_pressed("ui_accept") and velocity != Vector3(0, 0, 0):
+			speed = 30
+			dashing = true
+			$"Dash Timer".start()
 	
 	toBeNormalized = Vector2(horizontal, long).normalized()
 	velocity = Vector3(toBeNormalized.x * speed, 0, toBeNormalized.y * speed)
 	move_and_slide()
 
-func _update_speedometer():
-	curPos = sqrt(self.position.x ** 2 + self.position.z ** 2)
-	
-	distanceTraveled = curPos - oldPos
-	
-	movementSpeedCalc = abs(distanceTraveled) / $"Speedometer Timer".wait_time
-	
-	oldPos = curPos
-	
-	$"Speedometer Timer".start()
+func _dash_complete():
+	speed = 10
+	if horizontal == -1 and !Input.is_action_pressed("ui_left"): 
+		if Input.is_action_pressed("ui_right"):
+			horizontal = 1
+		else:
+			horizontal = 0
+		
+	if horizontal == 1 and !Input.is_action_pressed("ui_right"): 
+		if Input.is_action_pressed("ui_left"):
+			horizontal = -1
+		else:
+			horizontal = 0
+		
+	if long == 1 and !Input.is_action_pressed("ui_down"): 
+		if Input.is_action_pressed("ui_down"):
+			long = -1
+		else:
+			long = 0
+		
+	if long == -1 and !Input.is_action_pressed("ui_up"): 
+		if Input.is_action_pressed("ui_up"):
+			long = 1
+		else:
+			long = 0
+		
+	if horizontal == 0:
+		if Input.is_action_pressed("ui_left"):
+			horizontal = -1
+		if Input.is_action_pressed("ui_right"):
+			horizontal = 1
+			
+	if long == 0:
+		if Input.is_action_pressed("ui_up"):
+			long = -1
+		if Input.is_action_pressed("ui_down"):
+			long = 1
+			
+	dashing = false
