4499aa958694c9ee62b4349358665b79eea3714c
swivet-effect <dy.fry203@gmail.com>
2026-03-02 22:32:15 +1300

    Added Ghosts Picking Up / Dropping Objects - Fixed Ghost Queues

--- Objects/Ghost/Scripts/ghost.gd
+++ Objects/Ghost/Scripts/ghost.gd
@@ -1,35 +1,88 @@
 extends RigidBody3D
 
-var action
+# This is to replicate the player variables
+var dashing = false
+
 var randPos = Vector3(0, 0, 0)
 var speed = 5
 var rng = RandomNumberGenerator.new()
+var objects
+
+var carrying = false
+
+var pickupPos
+var pickupObject
 
 func _ready():
 	_choose_action()
 
 func _choose_action():
-	action = rng.randi_range(0, 1)
+	var action = rng.randi_range(0, 2)
+	
+	print("Action: " + str(action))
 	
 	match action:
 		0: 
 			_generate_random_pos(-50, 50, -50, 50)
-			_move_to()
+			$CurrentAction.text = "Going to " + str(randPos)
+			_move_to(randPos, "cont")
 		1:
+			$CurrentAction.text = "Waiting"
 			$"Rest Timer".start()
+		2:
+			_pickup()
 
 func _generate_random_pos(minX, maxX, minZ, maxZ):
 	randPos = Vector3(rng.randf_range(minX, maxX), 0.6, rng.randf_range(minZ,maxZ))
 
-func _move_to():
+func _move_to(pos, next):
 	var tween = get_tree().create_tween()
-	tween.connect("finished", _move_finished)
-	var travelTime = sqrt((randPos.x ** 2) + (randPos.z ** 2)) / speed
-	print(travelTime)
-	tween.tween_property(self, "position", randPos, travelTime)
+	
+	match next:
+		"cont": tween.connect("finished", _move_finished)
+		"grab": tween.connect("finished", _move_finished_grab)
+	
+	var travelTime = sqrt(((pos.x - self.position.x) ** 2) + ((pos.z - self.position.z) ** 2)) / speed
+	print("Movement speed: " + str(travelTime))
+	tween.tween_property(self, "position", pos, travelTime)
 
 func _move_finished():
+	print("Move to:" + str(randPos) + ", Stopped at: " + str(self.position))
+	_choose_action()
+	
+func _move_finished_grab():
+	print("Pickup at:" + str(pickupPos) + ", Stopped at: " + str(self.position))
+	pickupObject.carrier = self
+	pickupObject.isCarried = true
+	carrying = true
+	_choose_action()
+	
+func _rest_over():
+	print("Timer Out")
 	_choose_action()
 
-func _rest_over():
-	_choose_action()
+func _pickup():
+	
+	if carrying:
+		pickupObject.isCarried = false
+		pickupObject.position = Vector3(self.position.x, self.position.y, self.position.z)
+		_choose_action()
+	else:
+		# IF NO ITEMS ARE EVER CREATED OR DESTROYED THEN THIS SHOULD BE MOVED TO _READY
+		objects = get_tree().get_nodes_in_group("object")
+		
+		var findNearest = []
+		var pickedObject
+		
+		for i in range(objects.size()):
+			findNearest.push_back(sqrt(((objects[i].position.x - self.position.x) ** 2) + ((objects[i].position.z - self.position.z) ** 2)))
+		
+		pickedObject = findNearest.find(findNearest.min())
+		
+		$CurrentAction.text = "Picking up " + str(objects[pickedObject])
+		
+		pickupPos = Vector3(objects[pickedObject].position.x, 0.6, objects[pickedObject].position.z)
+		pickupObject = objects[pickedObject]
+		
+		_move_to(Vector3(objects[pickedObject].position.x, 0.6, objects[pickedObject].position.z), "grab")
+	
--- Objects/Pickups/Script/pickup.gd
+++ Objects/Pickups/Script/pickup.gd
@@ -5,6 +5,11 @@ extends RigidBody3D
 var canPickup = false
 var isCarried = false
 
+var possibleCarrier
+@onready var carrier = $"../Player"
+
+@onready var ghosts = get_tree().get_nodes_in_group("ghost")
+
 func _physics_process(_delta):
 	if linear_velocity == Vector3(0, 0, 0):
 		self.freeze = true
@@ -16,19 +21,20 @@ func _physics_process(_delta):
 	if Input.is_action_just_pressed("Interact"):
 		if canPickup:
 			isCarried = true
+			carrier = possibleCarrier 
 		elif isCarried:
 			isCarried = false
 			canPickup = true
-			self.position = Vector3($"../Player".position.x, $"../Player".position.y, $"../Player".position.z)
+			self.position = Vector3(carrier.position.x, carrier.position.y, carrier.position.z)
 	
-	if $"../Player".dashing and isCarried:
+	if carrier.dashing and isCarried:
 		isCarried = false
 		canPickup = true
-		self.position = Vector3($"../Player".position.x, $"../Player".position.y, $"../Player".position.z)
+		self.position = Vector3(carrier.position.x, carrier.position.y, carrier.position.z)
 	
 	if isCarried:
 		canPickup = false
-		self.position = Vector3($"../Player".position.x, $"../Player".position.y + 1, $"../Player".position.z)
+		self.position = Vector3(carrier.position.x, carrier.position.y + 1, carrier.position.z)
 	
 	if Input.is_action_pressed("Use") and isCarried:
 		match pickupType:
@@ -38,8 +44,14 @@ func _collision_enter(body: Node3D):
 	if body.is_in_group("player") and global.playerHovering == false:
 		global.playerHovering = true
 		canPickup = true
+		possibleCarrier = body
+	if body.is_in_group("ghost"):
+		canPickup = true
+		possibleCarrier = body
 
 func _collision_exit(body: Node3D):
 	global.playerHovering = false
 	if body.is_in_group("player"):
+		canPickup = false
+	if body.is_in_group("ghost"):
 		canPickup = false
