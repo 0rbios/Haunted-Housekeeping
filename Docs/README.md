2c0d1222edcda7bac90fb85c786fb479d85cb414
swivet-effect <dy.fry203@gmail.com>
2026-03-14 23:47:16 +1300

    A Note About Errors And Synchronisers

--- Docs/README.md
+++ Docs/README.md
@@ -1,2 +1,9 @@
 # Haunted Housekeeping
 ### An Arcade Cleaning Game with Ghosts
+
+> Note:
+> * When Re-connecting A Client, A Number Of Errors Equal To The Number Of
+MultiplayerSynchronizer Nodes In The Scene Will Occur. This Will Be Fixed In
+Future By Making All Synchronized Items (Ghosts, Objects, Dirt) Instantiated,
+Rather Than Hard Coded as Multiplayer Synchronizers Have Issues With Client
+Changes.
--- Multiplayer/multiplayer_handler.gd
+++ Multiplayer/multiplayer_handler.gd
@@ -3,6 +3,8 @@ extends Node
 const PORT = 6666
 const IPADDRESS = "localhost"
 
+var peer = ENetMultiplayerPeer.new()
+
 func _ready():
 	if MultiplayerSettings.clientType == "host":
 		create_server()
@@ -10,13 +12,11 @@ func _ready():
 		create_client()
 
 func create_server():
-	var peer = ENetMultiplayerPeer.new()
 	peer.create_server(PORT)
 	multiplayer.allow_object_decoding = true
 	multiplayer.multiplayer_peer = peer
 
 func create_client():
-	var peer = ENetMultiplayerPeer.new()
 	peer.create_client(IPADDRESS, PORT)
 	multiplayer.allow_object_decoding = true
 	multiplayer.multiplayer_peer = peer
