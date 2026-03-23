f7e62e83ff2bac060639d99de155503269f119b8
swivet-effect <dy.fry203@gmail.com>
2026-03-23 22:10:51 +1300

    Fixed Dirt Spawning

--- Multiplayer/dirt_manager.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@export var dirtState = []
-
-# Get A List Of All Dirt On The Map But Only On The Server
-func _ready():
-	if is_multiplayer_authority():
-		dirtState = get_tree().get_nodes_in_group("dirt")
--- Objects/Dirt/Scripts/dirt.gd
+++ Objects/Dirt/Scripts/dirt.gd
@@ -3,22 +3,10 @@ extends Area3D
 # Variables --------------------
 
 @export var cleanedBy: Array
-@onready var dirtManager = $".."
 
 # Functions ------------------
 
-func _process(_delta):
-	# Deletes Any Already Cleaned Dirt On Load
-	if !dirtManager.dirtState.has(self):
-		self.queue_free()
-
 func _collision_detected(body: Node3D):
 	if body.is_in_group("object") and Input.is_action_pressed("Use"):
-		if cleanedBy.has(body.pickupType):
-			_clean_dirt.rpc()
-
-@rpc("any_peer", "call_local")
-func _clean_dirt():
-	if is_multiplayer_authority():
-		dirtManager.dirtState.erase(self)
-		queue_free()
+		if cleanedBy.has(body.pickupType) and is_multiplayer_authority():
+			self.visible = false
