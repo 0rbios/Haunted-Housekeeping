ff7173b9b862b2d9439db34a77017124564b66a4
swivet-effect <dy.fry203@gmail.com>
2026-04-04 20:51:13 +1300

    Players Can Now Leave By Closing Game

--- Maps/Room/Scripts/load_room.gd
+++ Maps/Room/Scripts/load_room.gd
@@ -3,28 +3,45 @@ extends Control
 # Variables --------------------
 
 @onready var legend = get_tree().root.get_child(0)
-@onready var roomName = $"CanvasLayer/Room Name"
+@onready var roomNameText = $"CanvasLayer/Room Name"
 
 # Functions --------------------
 
 func _ready():
-	roomName.text = str(legend.activeRoom)
+	multiplayer.peer_disconnected.connect(player_disconnecting)
+	roomNameText.text = str(legend.activeRoom)
 
 func _player_leaving():
 	_leave_room.rpc_id(1, legend.activeRoom, multiplayer.get_unique_id())
 	legend.clean_tree()
+
+func player_disconnecting(id: int):
+	if multiplayer.get_unique_id() == 1:
+		for room in legend.rooms:
+			if room.players.has(str(id)):
+				if str(id) == room.owner:
+					if room.players.size() < 2:
+						legend.rooms.remove_at(legend.rooms.find(room))
+					else:
+						room.players.remove_at(room.players.find(str(id)))
+						room.owner = room.players[0]
+				else:
+					room.players.remove_at(room.players.find(str(id)))
+		print("Player Left Room, Updated Room List:")
+		print(legend.rooms)
 
 @rpc("any_peer", "call_local")
 func _leave_room(roomName, playerID):
 	for room in legend.rooms:
 		if room.name == roomName:
 			if str(playerID) == room.owner:
-				if room.players.size() > 1:
+				if room.players.size() < 2:
+					print(legend.rooms.find(room))
+					legend.rooms.remove_at(legend.rooms.find(room))
+				else:
 					room.players.remove_at(room.players.find(str(playerID)))
 					room.owner = room.players[0]
-				else:
-					legend.rooms.remove_at(legend.rooms.find(room))
 			else:
-				room.players.remove_at(room.players.find(playerID))
-	print("Player Left Room " + roomName + ", Updated Room List:")
+				room.players.remove_at(room.players.find(str(playerID)))
+	print("Player Left Room, Updated Room List:")
 	print(legend.rooms)
