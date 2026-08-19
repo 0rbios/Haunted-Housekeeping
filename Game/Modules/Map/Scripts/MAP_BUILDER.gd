b0c44435ad54a1e20c2ff3c00fecc260c509194e
Swivet-Effect <dy.fry203@gmail.com>
2026-08-19 21:45:04 +1200

    Players can no longer walk off map edge

--- Game/Modules/Map/Scripts/MAP_BUILDER.gd
+++ Game/Modules/Map/Scripts/MAP_BUILDER.gd
@@ -2,17 +2,23 @@ extends Node
 
 # Assembles the map. Including ghosts, dirt and players.
 
-var mLoad = preload("res://Game/Modules/Map/Scripts/MAP_LOADER.gd").new()
+var _mLoad = preload("res://Game/Modules/Map/Scripts/MAP_LOADER.gd").new()
 
 # Takes in the map data and applies it to the scene.
 func buildScene() -> void:
-	mLoad.loadMap(mLoad.readMap("res://Game/Maps/DEBUG.json"))
+	_mLoad.loadMap(_mLoad.readMap("res://Game/Maps/DEBUG.json"))
 
 func _ready() -> void:
+	var base = $Base
+	
 	buildScene()
 	
 	var player = load("res://Game/Modules/Player/Player.tscn").instantiate()
 	player.position = Vector3(0.0, 0.1, 0.0)
+	player.mapXPos = base.position.x + (base.scale.x / 2)
+	player.mapXNeg = base.position.x - (base.scale.x / 2)
+	player.mapZPos = base.position.z + (base.scale.z / 2)
+	player.mapZNeg = base.position.z - (base.scale.z / 2)
 	self.call_deferred("add_child", player)
 	
 	var camera = load("res://Game/Modules/Camera/Camera.tscn").instantiate()
--- Game/Modules/Player/Scripts/Player Movement.gd
+++ Game/Modules/Player/Scripts/Player Movement.gd
@@ -2,44 +2,54 @@ extends CharacterBody3D
 
 # Handle the player's movement around the scene
 
-@onready var groundCheck : Node = $"Ground Check"
+@onready var _groundCheck : Node = $"Ground Check"
 
-var canMove : bool = true
+var _canMove : bool = true
 
-var speed = 4
+var _speed = 4
 
-var xVel = 0
-var xKeys = []
+var _xVel = 0
+var _xKeys = []
 
-var zVel = 0
-var zKeys = []
+var _zVel = 0
+var _zKeys = []
 
-var gravity = -3
+var _gravity = -3
+
+var mapXPos = 5
+var mapXNeg = 5
+var mapZPos = 5
+var mapZNeg = 5
 
 func _physics_process(_delta: float) -> void:
-	canMove = groundCheck.is_colliding()
+	if position.x > mapXPos: position.x = mapXPos
+	if position.x < mapXNeg: position.x = mapXNeg
+	if position.z > mapZPos: position.z = mapZPos
+	if position.z < mapZNeg: position.z = mapZNeg
 	
-	if Input.is_action_just_pressed("ui_left"): xKeys.append(-1)
-	if Input.is_action_just_pressed("ui_right"): xKeys.append(1)
-	if Input.is_action_just_pressed("ui_up"): zKeys.append(-1)
-	if Input.is_action_just_pressed("ui_down"): zKeys.append(1)
+	_canMove = _groundCheck.is_colliding()
 	
-	if Input.is_action_just_released("ui_left"): xKeys.erase(-1)
-	if Input.is_action_just_released("ui_right"): xKeys.erase(1)
-	if Input.is_action_just_released("ui_up"): zKeys.erase(-1)
-	if Input.is_action_just_released("ui_down"): zKeys.erase(1)
+	if Input.is_action_just_pressed("ui_left"): _xKeys.append(-1)
+	if Input.is_action_just_pressed("ui_right"): _xKeys.append(1)
+	if Input.is_action_just_pressed("ui_up"): _zKeys.append(-1)
+	if Input.is_action_just_pressed("ui_down"): _zKeys.append(1)
 	
-	xVel = 0
-	for key in xKeys.size():
-		xVel += xKeys[key] * (key + 1)
+	if Input.is_action_just_released("ui_left"): _xKeys.erase(-1)
+	if Input.is_action_just_released("ui_right"): _xKeys.erase(1)
+	if Input.is_action_just_released("ui_up"): _zKeys.erase(-1)
+	if Input.is_action_just_released("ui_down"): _zKeys.erase(1)
 	
-	zVel = 0
-	for key in zKeys.size():
-		zVel += zKeys[key] * (key + 1)
+	_xVel = 0
+	for key in _xKeys.size():
+		_xVel += _xKeys[key] * (key + 1)
 	
-	var velNorm = Vector2(xVel * int(canMove), zVel * int(canMove)).normalized()
-	velocity = Vector3((velNorm.x * speed), gravity * int(!canMove), (velNorm.y * speed))
+	_zVel = 0
+	for key in _zKeys.size():
+		_zVel += _zKeys[key] * (key + 1)
 	
-	if (xKeys + zKeys).size() > 0:
+	var velNorm = Vector2(_xVel * int(_canMove), _zVel * int(_canMove)).normalized()
+	velocity = Vector3((velNorm.x * _speed), _gravity * int(!_canMove), (velNorm.y * _speed))
+	
+	if (_xKeys + _zKeys).size() > 0:
 		rotation.y = atan2(velocity.x * -1, velocity.z * -1)
 	move_and_slide()
